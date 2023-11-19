0a32063714100ae087ade5d9f6ec7647e1cf62b4
Osama <osamabinesmail@gmail.com>
2023-11-19 13:27:59 -0800

    formatted weather data more

--- app/page.tsx
+++ app/page.tsx
@@ -16,14 +16,15 @@ async function getTransit() {
     let stopNames: { [id: string]: string; } = {}
     if (!existsSync('localStops.json')) {
       const stopRes = await fetch(
-        `http://api.pugetsound.onebusaway.org/api/where/stops-for-location.json?key=${process.env.OBA_KEY}&lat=${process.env.LATITUDE}&lon=${process.env.LONGITUDE}&radius=${150}`
-      );
+        `http://api.pugetsound.onebusaway.org/api/where/stops-for-location.json?key=${process.env.OBA_KEY}&lat=${process.env.LATITUDE}&lon=${process.env.LONGITUDE}&radius=${150}`,
+        { cache: 'no-cache' }
+        );
       await sleep(10*1000);
       const stops = await stopRes.json().then(d => d['data']['list']);
       for (const currStop of stops) {
         stopNames[currStop['id']] = currStop['name'];
       }
-      await writeFile('localStops.json', JSON.stringify(stopNames)).catch(err => {
+      await writeFile('localStops.json', JSON.stringify(stopNames, null, 2)).catch(err => {
         console.log(err);
       });
     } else {
@@ -31,7 +32,8 @@ async function getTransit() {
     }
     console.log(stopNames)
     const arrivalsRes = await fetch(
-      `http://api.pugetsound.onebusaway.org/api/where/arrivals-and-departures-for-location.json?key=${process.env.OBA_KEY}&lat=${process.env.LATITUDE}&lon=${process.env.LONGITUDE}&latSpan=${0.0075}&lonSpan=${0.0075}`
+      `http://api.pugetsound.onebusaway.org/api/where/arrivals-and-departures-for-location.json?key=${process.env.OBA_KEY}&lat=${process.env.LATITUDE}&lon=${process.env.LONGITUDE}&latSpan=${0.0075}&lonSpan=${0.0075}`,
+      { cache: 'no-cache' }
     )
     await sleep(10*1000);
     const arrivals = await arrivalsRes.json().then(d => d['data']['entry']['arrivalsAndDepartures'].filter((arrival: {[id: string]: string}) => Object.keys(stopNames).includes(arrival['stopId'])));
@@ -46,22 +48,33 @@ async function getTransit() {
 async function getWeather() {
   try {
     const res = await fetch(
-      `https://api.weather.gov/points/${process.env.LATITUDE},${process.env.LONGITUDE}`
+      `https://api.weather.gov/points/${process.env.LATITUDE},${process.env.LONGITUDE}`,
+      { cache: 'no-cache' }
     );
     await sleep(1000);
     const data = await res.json().then(d => d['properties']);
-    const forecastRes = await fetch(data['forecastHourly']);
+    console.log(data['forecastHourly']);
+    const forecastRes = await fetch(
+      data['forecastHourly'],
+      { cache: 'no-cache' }
+    );
     const forecastData = await forecastRes.json();
     await writeFile('forecastData.json', JSON.stringify(forecastData, null, 2));
     const formattedForecast = [];
     for (const fc of forecastData['properties']['periods']) {
       formattedForecast.push({
         'number': fc.number,
-        'startTime': fc.startTime,
-        'endTime': fc.endTIme,
+        'startTime': new Date(fc.startTime).toLocaleTimeString(undefined, {
+          hour: 'numeric',
+          hour12: true,
+        }),
+        'endTime': new Date(fc.startTime).toLocaleTimeString(undefined, {
+          hour: 'numeric',
+          hour12: true,
+        }),
         'temperature': fc.temperature,
         'temperatureUnit': fc.temperatureUnit,
-        'precipitationProbability': fc.probabilityOfPreciptation.value,
+        'precipitationProbability': fc.probabilityOfPrecipitation.value,
         'humidity': fc.relativeHumidity.value,
         'windSpeed': fc.windSpeed,
         'windDirection': fc.windDirection,
@@ -69,6 +82,7 @@ async function getWeather() {
         'shortForecast': fc.shortForecast
       })
     }
+    await writeFile('formattedWeather.json', JSON.stringify(formattedForecast, null, 2));
   } catch (err) {
     console.error(err);
   }
