3117e9cea0cca66182436e686bdad48f63530a5c
Osama <osamabinesmail@gmail.com>
2023-11-28 23:17:52 -0800

    formatting weather & transit now; getting weather with icons

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,8 @@
 'use client'
 
-// import dotenv from 'dotenv';
-// import { writeFile } from 'fs/promises';
 import styles from './page.module.css';
 import { useEffect, useState } from 'react';
-// import { useEffect, useState } from 'react';
-
-// dotenv.config();
+import Image from 'next/image';
 
 interface arrivalJSON {
   predictedDepartureTime: number,
@@ -30,90 +26,66 @@ interface formattedArrivalJSON {
   tripStatus: JSON,
 }
 
+interface formattedWeatherJSON {
+  number: number,
+  startTime: string,
+  endTime: string,
+  temperature: number,
+  temperatureUnit: string,
+  precipitationProbability: number,
+  humidity: number,
+  windSpeed: string,
+  windDirection: string,
+  icon: string,
+  shortForecast: string
+}
+
 async function sleep(ms: number) {
   return await new Promise(resolve => setTimeout(resolve, ms));
 }
 
-async function getWeather() {
-  try {
-    const res = await fetch(
-      `https://api.weather.gov/points/${process.env.NEXT_PUBLIC_LATITUDE},${process.env.NEXT_PUBLIC_LONGITUDE}`,
-      {
-        cache: 'no-cache',
-      }
-    );
-    // await sleep(1000);
-    const data = await res.json().then(d => d['properties']);
-    console.log(data['forecastHourly']);
-    const forecastRes = await fetch(
-      data['forecastHourly'],
-      {
-        cache: 'no-cache',
-      }
-    );
-    const forecastData = await forecastRes.json();
-    // await writeFile('forecastData.json', JSON.stringify(forecastData, null, 2));
-    const formattedForecast = [];
-    const justHour = (x: string) => {
-      new Date(x).toLocaleTimeString(undefined, {
-        hour: 'numeric',
-        hour12: true,
-      })
-    };
-    for (const fc of forecastData['properties']['periods']) {
-      formattedForecast.push({
-        'number': fc.number,
-        'startTime': justHour(fc.startTime),
-        'endTime': justHour(fc.endTime),
-        'temperature': fc.temperature,
-        'temperatureUnit': fc.temperatureUnit,
-        'precipitationProbability': fc.probabilityOfPrecipitation.value,
-        'humidity': fc.relativeHumidity.value,
-        'windSpeed': fc.windSpeed,
-        'windDirection': fc.windDirection,
-        'icon': fc.icon,
-        'shortForecast': fc.shortForecast
-      })
-    }
-    // await writeFile('formattedWeather.json', JSON.stringify(formattedForecast, null, 2));
-  } catch (err) {
-    console.error(err);
-  }
-  return '';
-}
-
 export default function Home() {
-
   const [transitData, setTransitData] = useState<formattedArrivalJSON[]>();
-  const [isBusy, setIsBusy] = useState<boolean>(true);
-  useEffect(() => {
-    getTransit();
-  }, []);
+  const [transitBusy, setTransitBusy] = useState<boolean>(true);
 
-  const getTransit = async () => {
+  const [weatherData, setWeatherData] = useState<formattedWeatherJSON[]>();
+  const [weatherBusy, setWeatherBusy] = useState<boolean>(true);
+  useEffect(() => { 
+    getTransit({
+      1_11060: 'Broadway & E Denny Way',
+      1_11175: 'Broadway And Denny',
+      1_11180: 'Broadway  E & E John St',
+      1_29262: 'E John St & 10th Ave E',
+      1_29270: 'E John St & Broadway  E',
+      40_99603: 'Capitol Hill',
+      40_99610: 'Capitol Hill'
+  });
+    getWeather();
+  }, []);
+  const getTransit = async (stopNames: { [id: string]: string }) => {
     try {
-      let stopNames: { [id: string]: string; } = {}
-      const stops = await fetch(
-        `http://api.pugetsound.onebusaway.org/api/where/stops-for-location.json?key=${process.env.NEXT_PUBLIC_OBA_KEY}&lat=${process.env.NEXT_PUBLIC_LATITUDE}&lon=${process.env.NEXT_PUBLIC_LONGITUDE}&radius=${150}`,
-        {
-          cache: 'no-cache',
+      if (Object.keys(stopNames).length === 0) {
+        let stopNames: {[id: string]: string} = {};
+        const stops: [{[id: string]: string }]= await fetch(
+          `http://api.pugetsound.onebusaway.org/api/where/stops-for-location.json?key=${process.env.NEXT_PUBLIC_OBA_KEY}&lat=${process.env.NEXT_PUBLIC_LATITUDE}&lon=${process.env.NEXT_PUBLIC_LONGITUDE}&radius=${150}`,
+          {
+            cache: 'no-cache',
+          }
+        ).then(async stopRes => await stopRes.json()).then(d => d['data']['list']);
+        await sleep(30 * 1000);
+        for (const currStop of stops) {
+          stopNames[currStop['id']] = currStop['name'];
         }
-      ).then(async stopRes => await stopRes.json()).then(d => d['data']['list']);
-      for (const currStop of stops) {
-        stopNames[currStop['id']] = currStop['name'];
+        console.log(stopNames);
       }
-      // console.log(stopNames)
-      const arrivalsRes = await fetch(
+      const arrivals = await fetch(
         `http://api.pugetsound.onebusaway.org/api/where/arrivals-and-departures-for-location.json?key=${process.env.NEXT_PUBLIC_OBA_KEY}&lat=${process.env.NEXT_PUBLIC_LATITUDE}&lon=${process.env.NEXT_PUBLIC_LONGITUDE}&latSpan=${0.01}&lonSpan=${0.01}`,
         {
           cache: 'no-cache',
         }
-      )
-      // await sleep(10 * 1000);
-      const arrivals = await arrivalsRes.json().then(d => d['data']['entry']['arrivalsAndDepartures'].filter((arrival: arrivalJSON) => {
-        return Object.keys(stopNames).includes(arrival.stopId) && ((arrival.scheduledDepartureTime - Date.now()) < 60 * 60 * 1000) && ((Date.now() - arrival.predictedDepartureTime) <= 5*60*1000)
+      ).then(async arrivalsRes => await arrivalsRes.json()).then(d => d['data']['entry']['arrivalsAndDepartures'].filter((arrival: arrivalJSON) => {
+        return Object.keys(stopNames).includes(arrival.stopId) && ((arrival.scheduledDepartureTime - Date.now()) < 60 * 60 * 1000) && ((Date.now() - arrival.predictedDepartureTime) <= 5 * 60 * 1000)
       }));
-      // await writeFile('currentArrivals.json', JSON.stringify(arrivals, null, 2));
       const formattedArrivals: formattedArrivalJSON[] = [];
       const hoursMinutes = (x: string) => {
         return new Date(x).toLocaleTimeString(undefined, {
@@ -125,19 +97,19 @@ export default function Home() {
       for (const arrival of arrivals) {
         formattedArrivals.push(
           {
-            'blockTripSequence': arrival.blockTripSequence,
-            'lastUpdateTime': hoursMinutes(arrival.lastUpdateTime),
-            'numberOfStopsAway': arrival.numberOfStopsAway,
-            'predicted': arrival.predicted,
-            'predictedArrivalTime': hoursMinutes(arrival.predictedArrivalTime),
-            'predictedDepartureTime': hoursMinutes(arrival.predictedDepartureTime),
-            'routeShortName': arrival.routeShortName,
-            'scheduledArrivalTime': hoursMinutes(arrival.scheduledArrivalTime),
-            'scheduledDepartureTime': hoursMinutes(arrival.scheduledDepartureTime),
-            'stopId': arrival.stopId,
-            'stopSequence': arrival.stopSequence,
-            'tripHeadsign': arrival.tripHeadsign,
-            'tripStatus': arrival.tripStatus,
+            blockTripSequence: arrival.blockTripSequence,
+            lastUpdateTime: hoursMinutes(arrival.lastUpdateTime),
+            numberOfStopsAway: arrival.numberOfStopsAway,
+            predicted: arrival.predicted,
+            predictedArrivalTime: hoursMinutes(arrival.predictedArrivalTime),
+            predictedDepartureTime: hoursMinutes(arrival.predictedDepartureTime),
+            routeShortName: arrival.routeShortName,
+            scheduledArrivalTime: hoursMinutes(arrival.scheduledArrivalTime),
+            scheduledDepartureTime: hoursMinutes(arrival.scheduledDepartureTime),
+            stopId: arrival.stopId,
+            stopSequence: arrival.stopSequence,
+            tripHeadsign: arrival.tripHeadsign,
+            tripStatus: arrival.tripStatus,
           }
         )
       }
@@ -147,18 +119,66 @@ export default function Home() {
         }
         return 1;
       });
-      // await writeFile('formattedArrivals.json', JSON.stringify(sortedArrivals, null, 2));
       setTransitData(sortedArrivals);
-      setIsBusy(false);
+      setTransitBusy(false);
     } catch (err) {
       console.error(err);
     }
   }
 
+  const getWeather = async () => {
+    try {
+
+      await sleep(1000);
+      const data = await fetch(
+        `https://api.weather.gov/points/${process.env.NEXT_PUBLIC_LATITUDE},${process.env.NEXT_PUBLIC_LONGITUDE}`,
+        {
+          cache: 'no-cache',
+        }
+      ).then(async res => await res.json()).then(d => d['properties']);
+      // console.log(data['forecastHourly']);
+
+      await sleep(1000);
+      const forecastData = await fetch(
+        data['forecastHourly'],
+        {
+          cache: 'no-cache',
+        }
+      ).then(async forecastRes => forecastRes.json());
+
+      const formattedForecast: formattedWeatherJSON[] = [];
+      const justHour = (x: string) => {
+        return new Date(x).toLocaleTimeString(undefined, {
+          hour: 'numeric',
+          hour12: true,
+        })
+      };
+
+      for (const fc of forecastData['properties']['periods']) {
+        formattedForecast.push({
+          number: fc.number,
+          startTime: justHour(fc.startTime),
+          endTime: justHour(fc.endTime),
+          temperature: fc.temperature,
+          temperatureUnit: fc.temperatureUnit,
+          precipitationProbability: fc.probabilityOfPrecipitation.value,
+          humidity: fc.relativeHumidity.value,
+          windSpeed: fc.windSpeed,
+          windDirection: fc.windDirection,
+          icon: fc.icon,
+          shortForecast: fc.shortForecast
+        })
+      }
+      setWeatherData(formattedForecast);
+      setWeatherBusy(false);
+    } catch (err) {
+      console.error(err);
+    }
+  }
   return (
     <main>
       <div suppressHydrationWarning>
-        {(isBusy || !transitData) ? <div>Loading...</div> : <table className={styles.transitTable}><tbody>
+        {(transitBusy || !transitData) ? <div>Loading transit...</div> : <table className={styles.transitTable}><tbody>
           {transitData.map((a: formattedArrivalJSON, i: number) => {
             return <tr key={i}>
               <td>{a.routeShortName}</td>
@@ -167,9 +187,25 @@ export default function Home() {
             </tr>
           })}
         </tbody>
-        </table>
-        }
+        </table>}
+        {(weatherBusy || !weatherData) ? <div>Loading weather...</div> : <table><tbody>
+          {weatherData.map((b: formattedWeatherJSON, k: number) => {
+            return <tr key={k}>
+              <td>{b.startTime}</td>
+              <td>Temperature: {b.temperature}&#176; {b.temperatureUnit}</td>
+              <td>Chance of rain is {b.precipitationProbability}%</td>
+              <td><Image
+                alt={b.shortForecast}
+                width={100}
+                height={100}
+                src={b.icon} />
+              </td>
+            </tr>
+          })}
+        </tbody>
+        </table>}
       </div>
     </main>
   );
+
 }
