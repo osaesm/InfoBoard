5ee9e51db4193033d6b8651f11a2ae6b6f42a7e1
Osama <osamabinesmail@gmail.com>
2023-12-28 23:32:29 -0800

    i'm a dummy and remembered routes exist

--- api/server.js
+++ api/server.js
@@ -4,15 +4,13 @@ const cors = require('cors')
 const app = express()
 const port = 2384
 
-app.use(cors());
-
-app.get('/*', async (req, res) => {
+app.get('/transit*', cors(), async (req, res, next) => {
   const transitBaseUrl = 'http://api.pugetsound.onebusaway.org'
+  return res.status(200).json(await fetch(`${transitBaseUrl}${req.url.substring(8)}`).then(transitRes => transitRes.json()));
+})
+
+app.get('/weather*', async (req, res) => {
   const weatherBaseUrl = 'https://api.weather.gov'
-  // options are '/transit' and '/weather' both are 8 characters
-  if (req.url.substring(0, 8).localeCompare('/transit') == 0) {
-    return res.status(200).json(await fetch(`${transitBaseUrl}${req.url.substring(8)}`).then(transitRes => transitRes.json()));
-  }
   console.log(`${weatherBaseUrl}${req.url.substring(8)}`)
   return res.status(200).json(await fetch(`${weatherBaseUrl}${req.url.substring(8)}`).then(weatherRes => weatherRes.json()));
 })
