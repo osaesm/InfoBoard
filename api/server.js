98a9e3dd9a728e722f7ab53246dc86842bdd3587
Osama <osamabinesmail@gmail.com>
2023-12-28 16:12:37 -0800

    idk

--- api/server.js
+++ api/server.js
@@ -11,9 +11,9 @@ app.get('/*', async (req, res) => {
   const weatherBaseUrl = 'https://api.weather.gov'
   // options are '/transit' and '/weather' both are 8 characters
   if (req.url.substring(0, 8).localeCompare('/transit') == 0) {
-    console.log(req.url);
     return res.status(200).json(await fetch(`${transitBaseUrl}${req.url.substring(8)}`).then(transitRes => transitRes.json()));
   }
+  console.log(`${weatherBaseUrl}${req.url.substring(8)}`)
   return res.status(200).json(await fetch(`${weatherBaseUrl}${req.url.substring(8)}`).then(weatherRes => weatherRes.json()));
 })
 
